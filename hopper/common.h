b84176bae674fb16762eabb75f2dc8913eea4134
Pharap <2933055+Pharap@users.noreply.github.com>
2019-12-26 22:16:40 +0000

    Fix all warnings

--- hopper/common.h
+++ hopper/common.h
@@ -25,7 +25,7 @@ void drawLogo(void);
 void initTitle(void);
 bool updateTitle(void);
 void drawTitle(void);
-uint8_t setLastScore(int score, uint32_t time);
+uint8_t setLastScore(uint16_t score, uint32_t time);
 
 void initGame(void);
 bool updateGame(void);
